6563a959d73fcf4299a1fd05965e0950dd353ec3
hr-repo <hardikcollage2025@gmail.com>
2026-05-01 13:40:00 +0530

    Add files via upload

---
+++ quickSort.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class quickSort {
+    static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    static void quicksort(int arr[], int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quicksort(arr, low, pi - 1);
+            quicksort(arr, pi + 1, high);
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        Random r = new Random();
+        for (int i = 0; i < n; i++) {
+            arr[i] = r.nextInt(1000);
+        }
+
+        long start = System.nanoTime();
+        quicksort(arr, 0, n - 1);
+        long end = System.nanoTime();
+
+        System.out.println("Execution Time: " + (end - start));
+    }
+}
